1f92508bde534818ed1dab0446ebb5b9ae0cd03a
张荣平 <1473166181@qq.com>
2022-05-06 20:56:52 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,11 @@
 <template>
 	<div style="height: 400px">
 		<EditorText></EditorText>
-		<MyTable></MyTable>
 	</div>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
 import EditorText from './components/EditorText/EditorText.vue'
-import MyTable from './components/MyTable/MyTable.vue'
-import { TableOptions } from './components/MyTable/types'
-const tableData = [
-	{
-		date: '2016-05-03',
-		name: 'Tom',
-		address: 'No. 189, Grove St, Los Angeles',
-	},
-	{
-		date: '2016-05-02',
-		name: 'Tom',
-		address: 'No. 189, Grove St, Los Angeles',
-	},
-	{
-		date: '2016-05-04',
-		name: 'Tom',
-		address: 'No. 189, Grove St, Los Angeles',
-	},
-	{
-		date: '2016-05-01',
-		name: 'Tom',
-		address: 'No. 189, Grove St, Los Angeles',
-	},
-]
-
-const options: TableOptions[] = [
-	{
-		prop: 'date',
-		align: 'center',
-	},
-	{
-		prop: 'name',
-		align: 'center',
-	},
-	{
-		prop: 'address',
-		align: 'center',
-	},
-]
 </script>
 <style>
 #app {
