faf1846292a1847c9c11b0d38848d022f1a31f9a
张荣平 <1473166181@qq.com>
2022-05-06 22:10:22 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,71 @@
 <template>
 	<div style="height: 400px">
 		<EditorText></EditorText>
+		<EditTableVue :data="tableData" :options="options">
+			<template #action="{ scope }">
+				<el-button size="small" type="primary" @click="edit(scope)">编辑</el-button>
+				<el-button size="small" type="danger">删除</el-button>
+			</template>
+
+			<template #myname="{ scope }"> 哈哈 </template>
+		</EditTableVue>
 	</div>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
 import EditorText from './components/EditorText/EditorText.vue'
+import EditTableVue, { TableOptions } from './components/EditTable/EditTable.vue'
+
+const tableData = ref([
+	{
+		date: '2016-05-03',
+		name: 'Tom',
+		address: 'No. 189, Grove St, Los Angeles',
+	},
+	{
+		date: '2016-05-02',
+		name: 'Tom',
+		address: 'No. 189, Grove St, Los Angeles',
+	},
+	{
+		date: '2016-05-04',
+		name: 'Tom',
+		address: 'No. 189, Grove St, Los Angeles',
+	},
+	{
+		date: '2016-05-01',
+		name: 'Tom',
+		address: 'No. 189, Grove St, Los Angeles',
+	},
+])
+
+let options: TableOptions[] = [
+	{
+		label: '日期',
+		prop: 'date',
+		align: 'center',
+	},
+	{
+		label: '姓名',
+		prop: 'name',
+		align: 'center',
+		slot: 'myname',
+	},
+	{
+		label: '地址',
+		prop: 'address',
+		align: 'center',
+	},
+	{
+		label: '操作',
+		align: 'center',
+		action: true,
+	},
+]
+
+const edit = (scope: any) => {
+	console.log(scope)
+}
 </script>
 <style>
 #app {
---
+++ src/components/EditTable/EditTable.vue
@@ -0,0 +1,53 @@
+<template>
+	<el-table :data="data">
+		<template v-for="(item, index) in tableOptions" :key="index">
+			<el-table-column
+				v-if="!item.slot"
+				:label="item.label"
+				:align="item.align"
+				:prop="item.prop"
+			></el-table-column>
+			<el-table-column v-else :label="item.label" :align="item.align" :prop="item.prop">
+				<template #default="scope">
+					<slot :name="item.slot" :scope="scope"></slot>
+				</template>
+			</el-table-column>
+		</template>
+		<el-table-column :label="actionOptions!.label" :align="actionOptions!.align">
+			<template #default="scope">
+				<slot name="action" :scope="scope"></slot>
+			</template>
+		</el-table-column>
+	</el-table>
+</template>
+
+<script lang="ts" setup>
+import { isTemplateNode } from '@vue/compiler-core'
+import { ref, computed, defineProps, PropType } from 'vue'
+
+export interface TableOptions {
+	prop?: string
+	label: string
+	width?: string | number
+	align?: 'left' | 'center' | 'right'
+	slot?: string
+	action?: boolean
+}
+
+const props = defineProps({
+	options: {
+		type: Array as PropType<TableOptions[]>,
+		required: true,
+	},
+	data: {
+		type: Array as PropType<any[]>,
+		required: true,
+	},
+})
+
+let tableOptions = computed(() => props.options.filter((item) => item.action !== true))
+
+let actionOptions = computed(() => props.options.find((item) => item.action === true))
+</script>
+
+<style scoped lang="scss"></style>
