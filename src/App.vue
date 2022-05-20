48db509343ef03c56ef8916930706cebfd3b081a
张荣平 <1473166181@qq.com>
2022-05-20 09:18:04 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,22 @@
 <template>
 	<div style="height: 400px">
-		<EditorText></EditorText>
-		<!-- <EditTableVue :data="tableData" :options="options">
-			<template #action="{ scope }">
-				<el-button size="small" type="primary" @click="edit(scope)">编辑</el-button>
-				<el-button size="small" type="danger">删除</el-button>
-			</template>
-
-			<template #myname="{ scope }"> 哈哈 </template>
-		</EditTableVue> -->
-
+		<el-dialog title="ces" v-model="show">
+			<div class="box">
+				<EditorText v-if="a === 1"></EditorText>
+				<p v-else>sadasda</p>
+			</div>
+		</el-dialog>
+		<el-button @click="show = !show">展示</el-button>
+		<el-button @click="a = 2">2</el-button>
+		<el-button @click="a = 1">1</el-button>
 		<div class="lin"></div>
-		<!-- <MyTable :data="tableData2" @value-update="valeUpdate" @event-target="eventTarget"></MyTable> -->
-		<CustomTable
-			:data="tableData2"
-			@value-update="valeUpdate"
-			@event-target="eventTarget"
-		></CustomTable>
-
-		<div class="grid">
-			<div class="item" v-for="index in 6" :key="index">{{ index }}</div>
-		</div>
 	</div>
 </template>
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import EditorText from './components/EditorText/EditorText.vue'
-// import EditTableVue, { TableOptions } from './components/EditTable/EditTable.vue'
-
-import MyTable from './components/MyTable/MyTable.vue'
-import CustomTable from './components/CustomTable/CustomTable.vue'
-
-const tableData = ref([
-	{
-		date: '2016-05-03',
-		name: 'Tom',
-		address: 'No. 189, Grove St, Los Angeles',
-	},
-	{
-		date: '2016-05-02',
-		name: 'Tom',
-		address: 'No. 189, Grove St, Los Angeles',
-	},
-	{
-		date: '2016-05-04',
-		name: 'Tom',
-		address: 'No. 189, Grove St, Los Angeles',
-	},
-	{
-		date: '2016-05-01',
-		name: 'Tom',
-		address: 'No. 189, Grove St, Los Angeles',
-	},
-])
-
-const tableData2 = ref([
-	['2016-05-04', '测试', 'Tom', '', 'No. 189, Grove St, Los Angeles'],
-	['2016-05-05', '测试', 'Tom2', '', 'No. 189, Grove St, Los Angeles'],
-	['', '', '', '', ''],
-])
-
-const valeUpdate = (newVal, index, columnIndex) => {
-	tableData2.value[index][columnIndex] = newVal
-}
-
-const eventTarget = (eventText, index, columnIndex, editEvent) => {
-	editEvent[eventText](tableData2.value, Number(index), Number(columnIndex))
-}
-let options: TableOptions[] = [
-	{
-		label: '日期',
-		prop: 'date',
-		align: 'center',
-	},
-	{
-		label: '姓名',
-		prop: 'name',
-		align: 'center',
-		slot: 'myname',
-	},
-	{
-		label: '地址',
-		prop: 'address',
-		align: 'center',
-	},
-	{
-		label: '操作',
-		align: 'center',
-		action: true,
-	},
-]
-
-const edit = (scope: any) => {
-	console.log(scope)
-}
+const a = ref(1)
+const show = ref(true)
 </script>
 <style lang="scss">
 #app {
@@ -105,7 +28,9 @@ const edit = (scope: any) => {
 .lin {
 	margin-bottom: 40px;
 }
-
+.box {
+	height: 400px;
+}
 .grid {
 	display: grid;
 	grid-template-columns: 100px 100px;
