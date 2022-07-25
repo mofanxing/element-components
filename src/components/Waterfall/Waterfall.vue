1d7fa4a8b008f9b35587f0badf8cf4c7e7f0f88c
张荣平 <1473166181@qq.com>
2022-07-25 17:31:22 +0800

    fix

--- src/components/Waterfall/Waterfall.vue
+++ src/components/Waterfall/Waterfall.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="wrap">
-		<div class="container">
+		<div class="container" ref="container">
 			<div class="item" v-for="item in showList" :key="item.picture_id">
 				<img :src="item.original_url" />
 			</div>
@@ -9,10 +9,12 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import axios from 'axios'
 const list = ref<any[]>([])
 const showList = ref<any[]>([])
+const container = ref()
+let firstItemColumn: number[] = []
 const getList = async () => {
 	let { data } = await axios.get(
 		'https://www.vilipix.com/api/v1/picture/public?limit=50&tags=&sort=hot&offset=50'
@@ -21,6 +23,18 @@ const getList = async () => {
 	showList.value = list.value.slice(0, 10)
 }
 getList()
+
+const layoutItem = () => {
+	let ele = container.value.querySelectorAll('.item')
+	console.log(container.value.querySelectorAll('item'))
+}
+onMounted(() => {})
+watch(
+	() => showList.value,
+	() => {
+		layoutItem()
+	}
+)
 </script>
 
 <style scoped lang="scss">
