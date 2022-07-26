930dbb8107260a9347ce684b4d78d4ac140382f0
张荣平 <1473166181@qq.com>
2022-07-26 15:45:34 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
 	<div>
-		<Waterfall></Waterfall>
+		<!-- <Waterfall></Waterfall> -->
+
+		<KeepAlive>
+			<TestVue v-for="item in list" :id="item" :key="item">
+				<template #ss>
+					<div>{{ item }}</div>
+				</template>
+			</TestVue>
+		</KeepAlive>
+		<el-button @click="dele">删除</el-button>
+		<el-button @click="list.unshift(1)">添加</el-button>
 	</div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import Waterfall from './components/Waterfall/Waterfall.vue'
+import TestVue from './components/test/test.vue'
+const list = ref([0, 1, 2, 3, 4, 5])
+const dele = () => {
+	console.log(list.value.shift())
+}
 </script>
 <style lang="scss">
 #app {
---
+++ src/components/test/test.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<slot name="ss"> </slot>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { ref, defineProps, onActivated, PropType, onMounted } from 'vue'
+const props = defineProps({
+	id: {
+		type: Number as PropType<number>,
+	},
+})
+onMounted(() => {
+	console.log(props.id)
+})
+onActivated(() => {
+	console.log(props.id)
+})
+</script>
+
+<style scoped lang="scss"></style>
