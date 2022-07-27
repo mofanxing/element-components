5beaa1d395fabb42c90247af1e43e4cfaed23dd5
张荣平 <1473166181@qq.com>
2022-07-27 17:03:09 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,11 @@
 <template>
 	<div>
-		<KeepAlive>
-			<Test v-if="open" :list="list"></Test>
-		</KeepAlive>
-		<el-button @click="open = false">组件卸载</el-button>
-		<el-button @click="open = true">组件加载</el-button>
-		<el-button @click="updateList">更新</el-button>
-		<el-button @click="list.shift()">remove</el-button>
-		<el-button @click="list.push(0)">add</el-button>
+		<Waterfall></Waterfall>
 	</div>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
 import Waterfall from './components/Waterfall/Waterfall.vue'
-import Test from './components/test/test.vue'
-
-const list = ref([1, 2, 3])
-const updateList = () => {
-	list.value = [4, 5, 6]
-}
-const open = ref(true)
-const comIndex = ref('1')
-const thisCom = {
-	'1': Waterfall,
-}
 </script>
 <style lang="scss">
 #app {
--- src/components/Waterfall/Waterfall.vue
+++ src/components/Waterfall/Waterfall.vue
@@ -1,60 +1,77 @@
 <template>
 	<div class="wrap">
 		<div class="container" ref="container">
-			<div class="item" v-for="item in showList" :key="item.picture_id">
-				<img :src="item.original_url" />
+			<div class="item" :ref="getItem" v-for="item in showList" :key="item.picture_id">
+				<img :src="item.original_url" @load="setItemHeight" />
 			</div>
 		</div>
 	</div>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, watch } from 'vue'
+import { ref } from 'vue'
 import axios from 'axios'
 const list = ref<any[]>([])
 const showList = ref<any[]>([])
 const container = ref()
+const itemArr = ref([])
 let firstItemColumn: number[] = []
+const getItem = (el) => {
+	if (el) itemArr.value.push(el)
+}
+
 const getList = async () => {
 	let { data } = await axios.get(
 		'https://www.vilipix.com/api/v1/picture/public?limit=50&tags=&sort=hot&offset=50'
 	)
 	list.value = data.data.rows
-	showList.value = list.value.slice(0, 10)
+	showList.value = list.value.slice(0, 20)
 }
 getList()
 
-const layoutItem = () => {
-	let clientWidth = document.documentElement.clientWidth / 200
-	let itemWidth = container.value
-	console.log(itemWidth)
-	// container.value.style.width =
-}
-onMounted(() => {
-	layoutItem()
-})
-watch(
-	() => showList.value,
-	() => {
+const heightArr = ref<number[]>([])
+let imgLoadindex = 1
+const setItemHeight = () => {
+	if (imgLoadindex >= itemArr.value.length) {
 		layoutItem()
 	}
-)
+	imgLoadindex++
+}
+const layoutItem = () => {
+	let clientWidth = Math.floor(document.documentElement.clientWidth / 200)
+	container.value.style.width = 200 * clientWidth + 'px'
+	let arr = []
+	for (let i = 0; i < itemArr.value.length; i++) {
+		if (i < clientWidth) {
+			arr.push(itemArr.value[i].offsetHeight)
+			heightArr.value = arr
+			itemArr.value[i].style.transform = `translate(${i * 200}px, 0px)`
+		} else {
+			let minHeight = Math.min(...heightArr.value)
+			let minIndex = heightArr.value.findIndex((item) => item === minHeight)
+			let translateX = minIndex * 200
+			let translateY = minHeight
+			itemArr.value[i].style.transform = `translate(${translateX}px, ${translateY}px)`
+			heightArr.value[minIndex] = minHeight + itemArr.value[i].offsetHeight
+		}
+	}
+}
 </script>
 
 <style scoped lang="scss">
 .wrap {
-	padding: 20px;
-	box-sizing: border-box;
-	background-color: black;
+	width: 100vw;
+	height: 100vh;
+	background-color: #352c2e;
 	.container {
-		margin: 0 auto;
 		position: relative;
-		overflow: hidden;
+		height: 100%;
+		margin: 0 auto;
+		overflow-y: scroll;
 		.item {
 			width: 200px;
-
 			padding: 12px;
-			float: left;
+			position: absolute;
 			box-sizing: border-box;
 			img {
 				width: 100%;
